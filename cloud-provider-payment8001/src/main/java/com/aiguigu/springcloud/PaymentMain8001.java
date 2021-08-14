3fc31dd542fa92e1de9ff7072c579b639da10e51
libin <1638733229@qq.com>
2021-08-14 21:23:35 +0800

    first commit

---
+++ cloud-provider-payment8001/src/main/java/com/aiguigu/springcloud/PaymentMain8001.java
@@ -0,0 +1,17 @@
+package com.aiguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ：Li Bin
+ * @date ： 2021/8/14 21:07
+ * @description：
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
