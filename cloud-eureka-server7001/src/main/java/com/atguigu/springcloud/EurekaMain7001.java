866db04ade5521b69393e51171afc88fb822f7f4
libin <1638733229@qq.com>
2021-08-18 22:13:54 +0800

    公共模块+eureka。

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ：Li Bin
+ * @date ： 2021/8/18 22:06
+ * @description：
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
--- cloud-provider-payment8001/src/main/java/com/aiguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/aiguigu/springcloud/controller/PaymentController.java
@@ -4,10 +4,7 @@ import com.aiguigu.springcloud.entity.CommonResult;
 import com.aiguigu.springcloud.entity.Payment;
 import com.aiguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -27,7 +24,7 @@ public class PaymentController {
     private PaymentService paymentService;
 
     @PostMapping(value = "/payment/create")
-    public CommonResult create(Payment payment)
+    public CommonResult create(@RequestBody Payment payment)
     {
         int result = paymentService.create(payment);
         log.info("*****插入结果："+result);
